1909aad3a25d6eb5721b4a883517ae0413f9128e
AnvarKilyb <anvar.kily@gmail.com>
2023-08-08 14:03:42 +0200

    test

--- src/components/pages/events/Events.jsx
+++ src/components/pages/events/Events.jsx
@@ -1,5 +1,13 @@
 import styles from "./Events.module.css";
+import Wrapper from "../../../UI/Wrapper";
 const Events = () => {
-  return <h1>Events</h1>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>Events</h1>
+        <p>Some text below</p>
+      </div>
+    </Wrapper>
+  );
 };
 export default Events;
